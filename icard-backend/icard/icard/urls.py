1ecef539680a754014366ae08c8115f867efd45f
Alangobe <alangobe01@gmail.com>
2023-02-05 21:19:48 -0500

    Añadido drf_yasg

--- icard-backend/icard/icard/urls.py
+++ icard-backend/icard/icard/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='icard',
+        default_version='v1',
+        description='Carta digital para restaurantes'
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
