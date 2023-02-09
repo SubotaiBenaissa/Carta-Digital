10ce91e84016275195a31c330b42c8b39377ba44
Alangobe <alangobe01@gmail.com>
2023-02-08 20:11:19 -0500

    Iniciada configuración de rutas

--- icard-frontend/src/routes/routes-admin.js
+++ icard-frontend/src/routes/routes-admin.js
@@ -1,3 +1,12 @@
-const routesAdmin = []
+import { AdminLayout } from "../layouts"
+import { LoginAdmin } from "../pages"
+
+const routesAdmin = [
+    {
+        path: "/admin",
+        layout: AdminLayout,
+        component: LoginAdmin
+    }
+]
 
 export default routesAdmin
--- icard-frontend/src/routes/routes-client.js
+++ icard-frontend/src/routes/routes-client.js
@@ -1,3 +1,12 @@
-const routesClient = []
+import { ClientLayout } from "../layouts"
+import { Home } from "../pages"
+
+const routesClient = [
+    {
+        path: "/",
+        layout: ClientLayout,
+        component: Home,
+    }
+]
 
 export default routesClient
