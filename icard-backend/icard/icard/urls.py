1d3d881bcf86a44717c6373cf977b340b627ccea
Alangobe <alangobe01@gmail.com>
2023-02-13 20:03:39 -0500

    Añadido a rutas de la raíz

--- icard-backend/icard/icard/urls.py
+++ icard-backend/icard/icard/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from Usuario.router import router_user
@@ -32,4 +32,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/user', include(router_user.urls))
 ]
