34d9d84cdbbc726a7bcd9b2f519c8c27f3cc3e59
Alangobe <alangobe01@gmail.com>
2023-02-17 22:50:07 -0500

    Empezado con formik

--- icard-frontend/src/components/admin/loginform/LoginForm.jsx
+++ icard-frontend/src/components/admin/loginform/LoginForm.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import { Button, Form } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import "./LoginForm.scss"
+
+export function initialValuesForm(){ 
+
+    return {
+        email: "",
+        password: ""
+    }
+
+}
 
 export const LoginForm = () => {
 
+    const formik = useFormik({
+
+        initialValues: initialValuesForm(),
+
+        onSubmit: (formValue) => {
+            console.log("email enviado")
+            console.log(formValue)
+        }
+        
+    })
+
     return (
         <Form className="login-form-admin">
-            <Form.Input name="email" placeholder="Correo" />
-            <Form.Input name="password" type="password" placeholder="Contraseña"/>
+            <Form.Input name="email" placeholder="Correo" value={formik.values.email} onChange={formik.handleChange}/>
+            <Form.Input name="password" type="password" placeholder="Contraseña" value={formik.values.password} onChange={formik.handleChange}/>
             <Button type="submit" content="Iniciar sesión" primary fluid/> 
         </Form>
     )
