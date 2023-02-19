5e63c268242798908048c8d60fe700a68335a4c0
Alangobe <alangobe01@gmail.com>
2023-02-19 18:54:07 -0500

    Mostradas alertas de toast en login admin

--- icard-frontend/src/main.jsx
+++ icard-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import 'semantic-ui-css/semantic.min.css'
+import "react-toastify/dist/ReactToastify.css"
 import "./scss/global.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
