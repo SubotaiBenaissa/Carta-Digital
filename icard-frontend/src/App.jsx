4f87e1b1c403681323ae5e29c13fa1cc61fa5852
Alangobe <alangobe01@gmail.com>
2023-02-19 18:47:24 -0500

    Añadido ToastContainer al App.jsx

--- icard-frontend/src/App.jsx
+++ icard-frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { ToastContainer } from 'react-toastify'
 import { Navigation } from './routes/Navigation'
 
 export const App = () => {
   return (
     <div>
       <Navigation />
+      <ToastContainer 
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover={false}
+      />  
     </div>
   )
 }
