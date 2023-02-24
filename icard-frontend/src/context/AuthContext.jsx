aa06a8d4a5cba5fbef0382c54945697e877d7ee3
Alangobe <alangobe01@gmail.com>
2023-02-23 23:18:00 -0500

    Creado hook useUser

--- icard-frontend/src/context/AuthContext.jsx
+++ icard-frontend/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from 'react'
 import { setToken } from "../api/token"
+import { useUser } from "../hooks"
 
 export const AuthContext = createContext({
     auth: undefined,
@@ -9,8 +10,12 @@ export const AuthContext = createContext({
 
 export const AuthProvider = ({ children }) => {
 
+    const { getMe } = useUser();
+
     const login = async ( token ) => {
         setToken(token)
+        const Me = await getMe(token); 
+        console.log(Me)
         // console.log('Context login', token);
     }
 
---
+++ icard-frontend/src/hooks/useUser.js
@@ -0,0 +1,23 @@
+import { getMeApi } from "../api/user"
+
+export const useUser = () => {
+
+    const getMe = async(token) => {
+
+        try {
+
+            const response = await getMeApi(token);
+            return response
+
+        } catch (error) {
+
+            throw error;
+
+        }
+    }
+
+    return {
+        getMe,
+    }
+
+}
