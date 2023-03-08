89d2fdacc290c7043687b6bbc43c6f4f8e72cb10
Alangobe <alangobe01@gmail.com>
2023-03-07 21:18:02 -0500

    Modificado sidebar con nuevos items

--- icard-frontend/src/components/admin/sidemenu/SideMenu.jsx
+++ icard-frontend/src/components/admin/sidemenu/SideMenu.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Menu, Icon } from "semantic-ui-react"
+import { Link, useLocation } from 'react-router-dom'
 import "./SideMenu.scss"
 
-const MenuLeft = ({}) => {
+const MenuLeft = ({ pathname }) => {
 
     return (
         <Menu fixed="left" borderless className="side" vertical>
-            <Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin' }>
                 <Icon name="home" /> Pedidos
+            </Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin/table' }>
+                <Icon name="table" /> Mesas
+            </Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin/payments-history' }>
+                <Icon name="history" /> Historial de pagos
+            </Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin/categories' }>
+                <Icon name="folder" /> Categorías
+            </Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin/products' }>
+                <Icon name="cart" /> Productos
+            </Menu.Item>
+            <Menu.Item as={ Link } to={ '/admin/users' }>
+                <Icon name="users" /> Usuarios
             </Menu.Item>
         </Menu>
     )
@@ -16,10 +32,13 @@ const MenuLeft = ({}) => {
 
 export const SideMenu = ({ children }) => {
 
+    console.log(useLocation())
+    const { pathname } = useLocation()
+
     return (
 
         <div className="side-menu-admin">
-            <MenuLeft />
+            <MenuLeft pathname={ pathname }/>
             <div className="content">{ children }</div>
         </div>
 
