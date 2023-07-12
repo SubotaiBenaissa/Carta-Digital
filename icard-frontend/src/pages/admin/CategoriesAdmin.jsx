82da4d3d8f0beabfac7f97e25b00dfbf88485055
Alangobe <alangobe01@gmail.com>
2023-07-12 17:03:06 -0500

    Implementado hook de categorías en componente

--- icard-frontend/src/pages/admin/CategoriesAdmin.jsx
+++ icard-frontend/src/pages/admin/CategoriesAdmin.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Loader } from "semantic-ui-react"
 import { HeaderPage } from "../../components/admin"
+import { useCategory } from "../../hooks"
 
 export const CategoriesAdmin = () => {
+
+    const { loading, categories, getCategories } = useCategory()
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+    
+    console.log(categories)
 
     return (
         
         <>
             <HeaderPage title="Categorías" btnTitle="Nueva Categoría"/>
+            { loading ? (
+                <Loader active inline="centered">
+                    Cargando...
+                </Loader>
+            ) : (
+                <h2>Lista de categorías</h2>
+            ) }
         </>
 
     )
