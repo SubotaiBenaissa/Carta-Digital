158f4492ab51b9e5ed879538dec17eb118ac5467
Alangobe <alangobe01@gmail.com>
2023-07-12 15:28:50 -0500

    Empezada vista y creado serializador para Categoria

--- icard-backend/icard/Categorias/views.py
+++ icard-backend/icard/Categorias/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import CategoriaSerializer
+from .models import Categoria
 
 # Create your views here.
+class CategoriaModelViewSet(ModelViewSet):
+    
+    permission_classes = IsAuthenticatedOrReadOnly
+    serializer_class = CategoriaSerializer
+    queryset = Categoria.objects.all()
