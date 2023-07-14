491dad15f7583ed21b0ad614f3aba71e820b5d03
Alangobe <alangobe01@gmail.com>
2023-07-14 17:39:09 -0500

    Configurada selección de imagen con dropzone

--- icard-frontend/src/components/admin/category/addeditcategory/AddEditCategory.jsx
+++ icard-frontend/src/components/admin/category/addeditcategory/AddEditCategory.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { Form, Image, Button } from "semantic-ui-react"
 import { useDropzone } from "react-dropzone"
 import "./AddEditCategory.scss"
 
 export const AddEditCategory = () => {
 
+    const [previewImage, setPreviewImage] = useState(null)
+
+    const onDrop = useCallback(( imageFile ) => {
+        const file = imageFile[0]
+    }, [])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/jpeg, image/png',
+        noKeyboard: true,
+        multiple: false,
+        onDrop
+    })
+ 
     return (
 
         <Form className="add-edit-category-form">
             <Form.Input name="title" placeholder="Nombre de categoría" />
-            <Button type="button" fluid>Subir imagen</Button>
+            <Button type="button" fluid { ...getRootProps() }>Subir imagen</Button>
+            <input { ...getInputProps() } />
             <Button type="submit" primary fluid>Crear</Button>
         </Form>
     
