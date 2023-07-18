dd648a2dcadbe7fcc18d97556b7acba10edf218f
Alangobe <alangobe01@gmail.com>
2023-07-18 17:56:52 -0500

    Modificado serializador para mostrar datos de la categoría a la que corresponde un producto

--- icard-backend/icard/Productos/serializers.py
+++ icard-backend/icard/Productos/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from Categorias.serializers import CategoriaSerializer
 from .models import Producto
 
 class ProductoSerializer(ModelSerializer):
     
+    category_data = CategoriaSerializer(source='category', read_only=True)
+    
     class Meta:
         
         model = Producto
-        fields = ['id', 'title', 'image', 'price', 'active', 'category']
+        fields = ['id', 'title', 'image', 'price', 'active', 'category', 'category_data']
