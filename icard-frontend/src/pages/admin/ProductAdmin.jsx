b461dd9f0f341872409632394f55fbeda5a52398
Alangobe <alangobe01@gmail.com>
2023-07-19 09:36:46 -0500

    Implementada petición para traer productos

--- icard-frontend/src/pages/admin/ProductAdmin.jsx
+++ icard-frontend/src/pages/admin/ProductAdmin.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeaderPage } from '../../components/admin'
+import { useProduct } from '../../hooks'
 
 export const ProductAdmin = () => {
+
+    const { loading, products, getProducts } = useProduct()
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+    
+    console.log(products);
 
     return (
         <>
