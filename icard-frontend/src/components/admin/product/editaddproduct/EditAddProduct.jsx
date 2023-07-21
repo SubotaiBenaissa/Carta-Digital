af4c50dd7d98a90a74f7efca747ddc4f9c1f80be
Alangobe <alangobe01@gmail.com>
2023-07-21 10:46:15 -0500

    empezado a configurar dropdown en productos para categorías

--- icard-frontend/src/components/admin/product/editaddproduct/EditAddProduct.jsx
+++ icard-frontend/src/components/admin/product/editaddproduct/EditAddProduct.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { map } from 'lodash'
 import { Form, Image, Button, Dropdown, Checkbox } from "semantic-ui-react"
+import { useCategory } from '../../../../hooks'
 import './EditAddProduct.scss'
 
+function formatDropdownData( data ) {
+
+    return map(data, (item) => (
+        {
+            key: item.id,
+            text: item.title,
+            value: item.id
+        }
+    ))
+
+}
+
 export const EditAddProduct = ({ onClose }) => {
+
+    const { categories, getCategories } = useCategory()
+    const [categoriesFormat, setCategoriesFormat] = useState([])
+
+    useEffect(() => {
+        
+        getCategories()
+
+    }, [])
+
+    useEffect(() => {
+        
+        setCategoriesFormat(formatDropdownData(categories))
+
+    }, [categories])
 
     return (
     
         <Form className="add-edit-product-form">
             <Form.Input name="title" placeholder="Nombre del producto" />
             <Form.Input name="price" placeholder="Precio del producto" type="number" />
-            <Dropdown placeholder="Categoria" fluid search/>
+            <Dropdown placeholder="Categoria" fluid search options={ categoriesFormat }/>
             <div className="add-edit-product-form__active">
                 <Checkbox toggle />
                 Producto Activo
