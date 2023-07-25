4a876da6b73834553066f55ca242d5803c544578
Alangobe <alangobe01@gmail.com>
2023-07-25 16:15:09 -0500

    Iniciada implementación de hook en página de mesas

--- icard-frontend/src/pages/admin/TablesAdmin.jsx
+++ icard-frontend/src/pages/admin/TablesAdmin.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeaderPage } from '../../components/admin'
+import { useTable } from '../../hooks'
 
 export const TablesAdmin = () => {
+
+    const { tables, loading, getTables } = useTable()
+
+    useEffect(() => {
+        
+        getTables()
+
+    }, [])
+    
+    console.log(tables)
 
     return (
         <div>
