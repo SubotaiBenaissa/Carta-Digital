96640ad2e6c884359040d0bd7bb832a82b1ee1d1
Alangobe <alangobe01@gmail.com>
2023-07-25 11:42:29 -0500

    Creada página de mesas

---
+++ icard-frontend/src/pages/admin/TablesAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderPage } from '../../components/admin'
+
+export const TablesAdmin = () => {
+
+    return (
+        <div>
+            <HeaderPage title="Mesas" btnTitle="Crear nueva mesa"/>
+        </div>
+    )
+
+}
--- icard-frontend/src/pages/admin/index.js
+++ icard-frontend/src/pages/admin/index.js
@@ -1,3 +1,4 @@
 export * from './login'
 export * from './CategoriesAdmin'
 export * from './ProductAdmin'
+export * from './TablesAdmin'
