90181c4da071f783c235a1ae3e12f8d5fc26a577
Alangobe <alangobe01@gmail.com>
2023-07-27 11:47:08 -0500

    añadida aplicación al settings y creado el modelo para pedidos

--- icard-backend/icard/Pedidos/models.py
+++ icard-backend/icard/Pedidos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+Status = (
+    ("PENDIENTE", "pendiente"),
+    ("ENTREGADO", "entregado")
+)
+
 # Create your models here.
+class Pedido(models.Model):
+    
+    table = models.ForeignKey('Mesas.Mesa', on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey('Productos.Producto', on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=255, choices=Status)
+    created_at = models.DateTimeField(auto_now_add=True)
+    close = models.BooleanField(default=False)
+    
+    class Meta:
+        
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+        
+    def __str__(self):
+        
+        return str(self.table)
+        
--- icard-backend/icard/icard/settings.py
+++ icard-backend/icard/icard/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'Categorias',
     'Productos',
     'Mesas',
+    'Pedidos',
 ]
 
 MIDDLEWARE = [
