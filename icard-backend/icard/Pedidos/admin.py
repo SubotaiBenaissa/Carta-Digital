537a85c241014dd8e78dadea7caf6802114aebad
Alangobe <alangobe01@gmail.com>
2023-07-27 11:53:16 -0500

    Registrado modelo en el admin

--- icard-backend/icard/Pedidos/admin.py
+++ icard-backend/icard/Pedidos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pedido
 
 # Register your models here.
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    
+    list_display = ['table', 'product', 'status', 'created_at']
