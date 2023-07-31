ce01fd06eda69e12471988488c0706f2145ec14b
Alangobe <alangobe01@gmail.com>
2023-07-31 16:05:58 -0500

    Hechos filtros para pedidos

--- icard-backend/icard/Pedidos/views.py
+++ icard-backend/icard/Pedidos/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from .serializers import PedidoSerializer
 from .models import Pedido
 # Create your views here.
@@ -7,3 +9,6 @@ class PedidoModelViewSet(ModelViewSet):
     
     serializer_class = PedidoSerializer
     queryset = Pedido.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['table', 'status', 'close']
+    ordering_flieds = '__all__'
