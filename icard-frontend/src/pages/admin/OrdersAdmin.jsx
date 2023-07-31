3137f9c4e4e4f5d80eef221ba992d9dbce4cb572
Alangobe <alangobe01@gmail.com>
2023-07-31 16:59:39 -0500

    Empezado a configurar ordersadmin

--- icard-frontend/src/pages/admin/OrdersAdmin.jsx
+++ icard-frontend/src/pages/admin/OrdersAdmin.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Loader } from 'semantic-ui-react';
+import { HeaderPage } from "../../components/admin"
+import { useTable } from "../../hooks";
 
 export const OrdersAdmin = () => {
 
+    const { loading, tables, getTables } = useTable()
+
+    useEffect(() => {
+
+        getTables()
+
+    }, [])
+    
 
     return (
-        <div>
-            <h1>Orders Admin</h1>
-        </div>
+        <>
+            <HeaderPage title="Restaurante" />
+            {
+                loading ? (
+                    <Loader active inline="centered">
+                        Cargando...
+                    </Loader>
+                ) : (
+                    <h2>Lista mesas</h2>
+                )
+            }
+        </>
     )
 
 }
