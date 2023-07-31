78b7e6826ebda94b721f8194764a07911153f3e9
Alangobe <alangobe01@gmail.com>
2023-07-31 16:22:22 -0500

    Mostrados datos de mesa y producto en pedidos en el serializador

--- icard-backend/icard/Pedidos/serializers.py
+++ icard-backend/icard/Pedidos/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework.serializers import ModelSerializer
+from Productos.serializers import ProductoSerializer
+from Mesas.serializers import MesaSerializer
 from .models import Pedido
 
 class PedidoSerializer(ModelSerializer):
     
+    product_data = ProductoSerializer(source='product', read_only=True)
+    table_data = MesaSerializer(source='table', read_only=True)
+    
     class Meta: 
         
         model = Pedido
-        fields = ['id', 'table', 'status', 'product', 'close', 'created_at']
+        fields = ['id', 'table', 'table_data', 'status', 'product', 'product_data', 'close', 'created_at']
