90b89ad82655e863e48dc624de14bb59a0d6f7cd
Alangobe <alangobe01@gmail.com>
2023-08-01 09:45:47 -0500

    Creado componente table list

--- icard-frontend/src/components/admin/table/index.js
+++ icard-frontend/src/components/admin/table/index.js
@@ -1,2 +1,3 @@
 export * from './tableadmin'
 export * from './editaddtable'
+export * from './tablelist'
---
+++ icard-frontend/src/components/admin/table/tablelist/TableList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./TableList.scss"
+
+export const TableList = () => {
+
+    return (
+
+        <div>
+            <h1>Lista de mesas</h1>
+        </div>
+
+    )
+
+}
