d7ed6e8d77c4ecbbc5e2c66e7ea4c17faf7c787e
Alangobe <alangobe01@gmail.com>
2023-08-01 11:33:45 -0500

    añadidos estilos a table item

--- icard-frontend/src/components/admin/table/tableitem/TableItem.jsx
+++ icard-frontend/src/components/admin/table/tableitem/TableItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Label, Button, Icon, Checkbox } from 'semantic-ui-react'
 import tablesvg from '../../../../assets/table.svg'
+import "./TableItem.scss"
 
 export const TableItem = ({ table }) => {
 
