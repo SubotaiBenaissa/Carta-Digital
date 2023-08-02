7dd82f31496cdd333f853c3290f82dd61f849ec7
Alangobe <alangobe01@gmail.com>
2023-08-02 17:09:00 -0500

    Añadidos estatus y petición a pedidos al componente table item

--- icard-frontend/src/components/admin/table/tableitem/TableItem.jsx
+++ icard-frontend/src/components/admin/table/tableitem/TableItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Label, Button, Icon, Checkbox } from 'semantic-ui-react'
+import { getOrderByTableAPI } from '../../../../api/orders'
+import { OrderStatus } from '../../../../utils/constants'
 import tablesvg from '../../../../assets/table.svg'
 import "./TableItem.scss"
 
+
 export const TableItem = ({ table }) => {
+
+    useEffect(() => {
+        (async () => {
+            console.log(table.id)
+            const response = await getOrderByTableAPI(table.id, OrderStatus.PENDIENTE)
+            console.log(response)
+        })()
+    })
 
     return (
         <div className="table-item">
--- icard-frontend/src/utils/constants.js
+++ icard-frontend/src/utils/constants.js
@@ -1,2 +1,6 @@
 export const BASE_PATH = "http://127.0.0.1:8000"
 export const TOKEN = "token"
+export const OrderStatus = {
+    PENDIENTE: "PENDIENTE",
+    ENTREGADO: "ENTREGADO"
+}
