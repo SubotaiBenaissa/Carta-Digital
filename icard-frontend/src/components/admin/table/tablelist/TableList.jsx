fae3a05a47991ad29cbf81b66790b873d37dc6f3
Alangobe <alangobe01@gmail.com>
2023-08-02 11:08:48 -0500

    Cambiados estilos para Table List y añadido checkbox y botón de reload

--- icard-frontend/src/components/admin/table/tablelist/TableList.jsx
+++ icard-frontend/src/components/admin/table/tablelist/TableList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Icon, Checkbox } from "semantic-ui-react"
 import { map, size } from 'lodash'
 import { TableItem } from '../tableitem'
 import "./TableList.scss"
@@ -8,6 +9,13 @@ export const TableList = ({ tables }) => {
     return (
 
         <div className="tables-list-admin">
+            <Button primary icon className="tables-list-admin__reload" onClick={() => console.log('Reload')}>
+                <Icon name="refresh"></Icon>
+            </Button>
+            <div className="tables-list-admin__reload-toggle">
+                <span>Reload automático</span>
+                <Checkbox toggle onChange={ (_, data) => console.log(data.checked) }/>
+            </div>
             {
                 map(tables, (table) => (
                     <TableItem key={ table.number } table={ table }/>
