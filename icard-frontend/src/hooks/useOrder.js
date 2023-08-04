f157070dc543dad05d94cbf620b68b33523bd2a8
Alangobe <alangobe01@gmail.com>
2023-08-04 16:59:53 -0500

    Creado hook useOrder

---
+++ icard-frontend/src/hooks/useOrder.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { getOrderByTableAPI } from "../api/orders"
+
+export function useOrder() {
+
+    const [loading, setLoading] = useState(true)
+    const [errors, setErrors] = useState(false)
+    const [orders, setOrders] = useState(null)
+
+    const getOrderByTable = async(id, status, ordering) => {
+
+        try {
+
+            setLoading(true)
+            const response = await getOrderByTableAPI(id, status, ordering)
+            setLoading(false)
+            setOrders(response)
+            
+        } catch (error) {
+
+            setLoading(false)
+            setErrors(error)
+
+        }
+
+    }
+
+    return { 
+        getOrderByTable
+    }
+
+}
