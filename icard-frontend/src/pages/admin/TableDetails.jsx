47ff4c7341b9a2580cc34081e7ee3896c3c3ec0b
Alangobe <alangobe01@gmail.com>
2023-09-10 19:15:38 -0500

    Solucionado pequeño error y extraída cantidad de pedidos

--- icard-frontend/src/pages/admin/TableDetails.jsx
+++ icard-frontend/src/pages/admin/TableDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Loader } from "semantic-ui-react"
 import { useOrder } from "../../hooks"
 import { HeaderPage } from "../../components/admin"
+import { ListOrder } from "../../components/admin"
 import { useParams } from "react-router-dom"
 
 export const TableDetails = () => {
@@ -14,19 +15,21 @@ export const TableDetails = () => {
     }, [])
 
     console.log(orders)
+    console.log(loading)
     
     return (
-        <HeaderPage title="Mesa">
-            {
-                loading ? (
-                    <Loader active inline="centered">
-                        Cargando...
-                    </Loader>
-                ) : (
-                    <h2>Lista pedidos</h2>
-                )
-            }
-        </HeaderPage>
+        <>
+            <HeaderPage title={"Mesa"} />
+                {
+                    loading ? (
+                        <Loader active inline="centered">
+                            Cargando...
+                        </Loader>
+                    ) : (
+                        <ListOrder orders={ orders } />
+                    )
+                }
+        </>
     )
 
 }
