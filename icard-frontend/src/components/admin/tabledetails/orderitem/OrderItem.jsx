75723b65289a697bdb83fe6d983fd7fd97705eec
Alangobe <alangobe01@gmail.com>
2023-09-14 10:50:23 -0500

    Estilizado listado de pedidos por mesa

--- icard-frontend/src/components/admin/tabledetails/orderitem/OrderItem.jsx
+++ icard-frontend/src/components/admin/tabledetails/orderitem/OrderItem.jsx
@@ -4,12 +4,18 @@ import "./OrderItem.scss"
 
 export const OrderItem = ({ order }) => {
 
+    const { title, image } = order.product_data;
+
     return (
-        <div className="order-item">
+        <div className="order-item-admin">
             <div className="order-item-admin__time">
                 {
                     order.created_at
                 }
+            </div>
+            <div className="order-item-admin__product">
+                <Image src={ image } />
+                <p>{ title }</p>
             </div>
         </div>
     )
