236e09155bbd4a07487924928b93f51917c1a037
Alangobe <alangobe01@gmail.com>
2023-09-15 17:42:01 -0500

    Configurados productos en dropdown de crear pedido

--- icard-frontend/src/components/admin/orders/addorderform/AddOrderForm.jsx
+++ icard-frontend/src/components/admin/orders/addorderform/AddOrderForm.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Image, Button, Dropdown } from "semantic-ui-react"
+import { map } from 'lodash'
+import { useProduct } from "../../../../hooks"
 import "./AddOrderForm.scss"
+
+function formatDropdownData(data) {
+
+    return map(data, (item) => ({
+
+        key: item.id,
+        text: item.title,
+        value: item.id
+
+    }))
+
+}
 
 export const AddOrderForm = ({ id, openCloseModal }) => {
 
+    const { products, getProducts } = useProduct()
+    const [productFormat, setProductFormat] = useState([])
+
+    console.log(productFormat)
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    useEffect(() => {
+        setProductFormat(formatDropdownData(products))
+    }, [products])
+
     return (
         <Form className="add-order-form">
-            <Dropdown placeholder="Productos" fluid selection search />
+            <Dropdown placeholder="Productos" fluid selection search options={ productFormat } />
             <div className='add-order-form__list'>
                 {
                     // Productos seleccionados
