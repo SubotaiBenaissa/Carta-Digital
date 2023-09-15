b259099b0ca1243d73de0bc3f3eac81dacfe9da2
Alangobe <alangobe01@gmail.com>
2023-09-15 11:52:11 -0500

    Creada petición para traer una sola mesa

--- icard-frontend/src/api/tables.js
+++ icard-frontend/src/api/tables.js
@@ -23,6 +23,23 @@ export async function getTablesAPI( token ) {
 
 }
 
+export async function getTableAPI(id) {
+
+    try {
+
+        const url = `${BASE_PATH}/api/tables/${id}/`;
+        const response = await fetch(url);
+        const result = await response.json()
+        return result
+        
+    } catch (error) {
+        
+        throw error
+
+    }
+
+}
+
 export async function addTableAPI( data, token ) {
 
     try {
