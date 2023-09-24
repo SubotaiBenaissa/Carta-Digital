456a5f787684a5a0232d5a9ec735001da67661c3
Alangobe <alangobe01@gmail.com>
2023-09-24 18:34:18 -0500

    Creado sistema de filtro para Pagos

--- icard-backend/icard/Pagos/views.py
+++ icard-backend/icard/Pagos/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import PagoSerializer
 from .models import Pago
 
@@ -8,3 +10,6 @@ class PagoModelApiViewSet(ModelViewSet):
     
     serializer_class = PagoSerializer
     queryset = Pago.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['mesa', 'estadoPago']
+    ordering_fields = '__all__'
