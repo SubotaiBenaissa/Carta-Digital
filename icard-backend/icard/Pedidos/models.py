b2bc1f80657f83dc1e1ef6289d7fa45b99698399
Alangobe <alangobe01@gmail.com>
2023-09-24 18:45:47 -0500

    Modificado modelo de pedidos para agregar pagos

--- icard-backend/icard/Pedidos/models.py
+++ icard-backend/icard/Pedidos/models.py
@@ -10,6 +10,7 @@ class Pedido(models.Model):
     
     table = models.ForeignKey('Mesas.Mesa', on_delete=models.SET_NULL, null=True, blank=True)
     product = models.ForeignKey('Productos.Producto', on_delete=models.SET_NULL, null=True, blank=True)
+    payment = models.ForeignKey('Pagos.Pago', on_delete=models.CASCADE, null=True, blank=True)
     status = models.CharField(max_length=255, choices=Status)
     created_at = models.DateTimeField(auto_now_add=True)
     close = models.BooleanField(default=False)
