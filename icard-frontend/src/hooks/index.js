4795faeca0361173d25109ac2a536f2833c425b4
Alangobe <alangobe01@gmail.com>
2023-09-28 11:09:13 -0500

    Creado hook usePayment

--- icard-frontend/src/hooks/index.js
+++ icard-frontend/src/hooks/index.js
@@ -4,3 +4,4 @@ export * from './useCategory'
 export * from './useProduct'
 export * from './useTables'
 export * from './useOrder'
+export * from "./usePayment"
---
+++ icard-frontend/src/hooks/usePayment.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { createPaymentAPI } from "../api/payment"
+
+export function usePayment() {
+
+    const [error, setError] = useState(null)
+
+    const createPayment = async(paymentData) => {
+
+        try {
+
+            return await createPaymentAPI(paymentData)
+            
+        } catch (error) {
+            
+            setError(error)
+
+        }
+
+    }
+
+    return {
+
+
+
+    }
+
+}
