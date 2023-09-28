4440f805adfd7f9459f872fbbe018281af777c47
Alangobe <alangobe01@gmail.com>
2023-09-28 11:01:36 -0500

    Creada petición para pagos

---
+++ icard-frontend/src/api/payment.js
@@ -0,0 +1,26 @@
+import { BASE_PATH } from "../utils/constants";
+
+export async function createPaymentAPI( paymentData ) {
+
+    try {
+
+        const url = `${BASE_PATH}/api/pagos/`
+        const params = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(paymentData)
+        }
+
+        const response = await fetch(url, params);
+        const result = await response.json()
+        return result
+        
+    } catch (error) {
+        
+        throw error
+
+    }
+
+}
