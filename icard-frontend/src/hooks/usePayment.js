ef0aef164dc2a1f65eda234aaa08c9be3a9340f9
Alangobe <alangobe01@gmail.com>
2024-01-24 11:00:40 -0500

    Creada función getPayments en hook usePayment

--- icard-frontend/src/hooks/usePayment.js
+++ icard-frontend/src/hooks/usePayment.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
-import { createPaymentAPI, getPaymentByTableAPI, closePaymentAPI } from "../api/payment"
+import { createPaymentAPI, getPaymentByTableAPI, closePaymentAPI, getPaymentsAPI } from "../api/payment"
 
 export function usePayment() {
 
     const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [payments, setPayments] = useState(null)
 
     const createPayment = async(paymentData) => {
 
@@ -47,9 +49,30 @@ export function usePayment() {
 
     }
 
+    const getPayments = async(idPayment) => {
+
+        try {
+            
+            setLoading(true)
+            const response = await getPaymentsAPI()
+            setLoading(false)
+            setPayments(response)
+
+        } catch (error) {
+            
+            setLoading(false)
+            setError(error)
+
+        }
+
+    }
+
     return {
 
         error,
+        payments,
+        loading,
+        getPayments,
         createPayment,
         getPaymentByTable,
         closePayment
--- icard-frontend/src/pages/admin/PaymentHistory.jsx
+++ icard-frontend/src/pages/admin/PaymentHistory.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeaderPage } from "../../components/admin"
+import { usePayment } from "../../hooks"
+import { Loader } from 'semantic-ui-react';
 
 export const PaymentHistory = () => {
+
+    const { loading, payments, getPayments } = usePayment();
+
+    useEffect(() => {
+        getPayments()
+    }, [])
+
+    console.log(payments)
 
     return (
         <>
             <HeaderPage title="Historial de pagos" />
+            {
+                loading ? (
+                    <Loader active inline>
+                        Cargando...
+                    </Loader>
+                ) : (
+                    <h2>Tabla de pagos</h2>
+                )
+            }
         </>
     )
 
