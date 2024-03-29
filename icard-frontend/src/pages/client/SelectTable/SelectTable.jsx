b470d765f5b7e3c6d5396c2666c825f8872879a1
Alangobe <alangobe01@gmail.com>
2024-03-28 21:57:15 -0500

    Hecha validación de error en el formulario de inicio

--- icard-frontend/src/pages/client/SelectTable/SelectTable.jsx
+++ icard-frontend/src/pages/client/SelectTable/SelectTable.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
 import './SelectTable.scss'
 import { Form, Button } from "semantic-ui-react"
 
 export const SelectTable = () => {
 
+    const [tableNum, setTableNum] = useState(null)
+    const [error, setError] = useState(null)
+
+    const onSubmit = () => {
+
+        setError(null)
+
+        if ( !tableNum ) {
+            setError("No se ha seleccionado ninguna mesa")
+        } else {
+            console.log("entrando xddd")
+        }
+        
+    }
+ 
     return (
         <div className="select-table">
             <div className="select-table__content">
                 <h1>Bienvenido a la carta digital</h1>
                 <h2>Introduce tu número de la mesa</h2>
+                <Form onSubmit={ onSubmit }>
+                    <Form.Input placeholder="Ejemplo: 1, 2, 3" type='number' onChange={(_, data) => setTableNum(data.value)} />
+                    <Button primary fluid>
+                        Entrar
+                    </Button>
+                </Form>
+                <p className='select-table__content-error'>
+                    { error }
+                </p>
             </div>
         </div>
     )
