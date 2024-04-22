a3337b47e1cb92dac02009fedad1f0bbefcef5bb
Alangobe <alangobe01@gmail.com>
2024-04-21 22:15:13 -0500

    Dirigido a path de categoría

--- icard-frontend/src/components/client/listcategories/ListCategories.jsx
+++ icard-frontend/src/components/client/listcategories/ListCategories.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Image } from "semantic-ui-react"
 import { map } from "lodash"
+import { useLocation, useNavigate } from "react-router-dom"
 import "./ListCategories.scss"
 
 export const ListCategories = ({ categories }) => {
 
-    
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const goToCategory = (id) => {
+        navigate(`${ location.pathname }/${ id }`)
+    }
+
+    console.log(location)
 
     return (
 
         <div className="list-categories-client">
             {
                 map(categories, (category) => (
-                    <div key={ category.id } className='list-categories-client__category'>
+                    <div key={ category.id } className='list-categories-client__category' onClick={ () => goToCategory(category.id) }>
                         <Image src={ category.image } size="small" />
                         <span>{ category.title }</span>
                     </div>
