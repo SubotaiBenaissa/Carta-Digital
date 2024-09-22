337cd23bc9823fdb16133010a63d35c7e1b69e7f
Alangobe <alangobe01@gmail.com>
2024-09-22 18:40:39 -0500

    Establecidas condiciones de productos para el listado

--- icard-frontend/src/pages/client/Cart.jsx
+++ icard-frontend/src/pages/client/Cart.jsx
@@ -1,25 +1,50 @@
 import { useEffect, useState } from 'react'
 import { useProduct } from '../../hooks' 
+import { Link, useParams } from "react-router-dom"
 import { getProductsCart } from "../../api/cart"
+import { size } from 'lodash'
+import { Button } from 'semantic-ui-react'
 
 export const Cart = () => {
 
+    const { getProductByID } = useProduct()
+    const [ products, setProducts ] = useState(null)
+    const { tableNumber } = useParams();
+
     const getListProducts = async() => {
+
         const idProductsCart = getProductsCart()
         const productsArray = []
+
         for await (const idProduct of idProductsCart) {
-            console.log(idProduct)
+            const response = await getProductByID(idProduct)
+            productsArray.push(response)
         }
+
+        setProducts(productsArray);
+
     }
 
     useEffect(() => {
         getListProducts()
     }, [])
     
-
     return (
         <div>
             <h1>Carrito</h1>
+            {
+                !products ? (
+                    <p>Cargando...</p>
+                ): size(products) < 1 ? (
+                    <div style={{ textAlign: 'center' }}>
+                        <p>Carrito vacío</p>
+                        <Link to={`/client/${tableNumber}/orders`}>
+                            <Button primary>Ver pedidos</Button>
+                        </Link>
+                    </div>
+                ): 
+                <p>Lista de productos</p>
+            }
         </div>
     )
 }
