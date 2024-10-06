8fb7c56ee7904a116be4133b891689f9558523ff
Alangobe <alangobe01@gmail.com>
2024-10-05 21:38:00 -0500

    listado numero de pedidos para la mesa

--- icard-frontend/src/pages/client/OrdersHistory.jsx
+++ icard-frontend/src/pages/client/OrdersHistory.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom"
+import { useOrder, useTable } from '../../hooks'
 
 export const OrdersHistory = () => {
 
-    return (
-        <div>OrdersHistory</div>
-    )
+    const { loading, orders, getOrderByTable } = useOrder()
+    const { getTableByNumber } = useTable()
+    const { tableNumber } = useParams()
+
+    const getTableInfo = async() => {
+
+        const table = await getTableByNumber(tableNumber)
+        const idTable = table[0].id
+        getOrderByTable(idTable, "", "ordering=-status,-created_at")
+
+    }
+
+    useEffect(() => {
+        getTableInfo()
+    }, [])
+
+    console.log(orders)
     
+    return (
+        <div>
+            <h1>Historial de pedidos</h1>
+        </div>
+    )
+
 }
